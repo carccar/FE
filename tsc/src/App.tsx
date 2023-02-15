8893e5b31f79cb997e5185a7e8713ef76f6efe28
aran <aran@aranui-MacBookAir.local>
2023-02-16 00:05:23 +0900

    tsc 컴포넌트 분리 예제 연습

--- tsc/src/App.tsx
+++ tsc/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import CountryList from './CountryList';
+
+export type CountryType = {
+  no: number;
+  country: string;
+  visited: boolean;
+}
+
+const App = ()  => {
+  // let list:Array<CountryType> = [
+  //   { no: 1, country: "한국", visited: true },
+  //   { no: 2, country: "파리", visited: true },
+  //   { no: 3, country: "미국", visited: false },
+  //   { no: 4, country: "대만", visited: true },
+  // ];
+
+  // let  msg = "<i>World</i>";
+  // let  msg = (<i>World</i>);
+
+  const [msg, setMsg] = useState<string>('world');
+  const [list, setList] = useState<Array<CountryType>>([
+      { no: 1, country: "한국", visited: true },
+      { no: 2, country: "파리", visited: true },
+      { no: 3, country: "미국", visited: false },
+      { no: 4, country: "대만", visited: true },
+  ])
+
+  const  addResult = (x: number, y: number) => {
+    return (
+      <div className='calc'>
+        {x} + {y} = {x+y}
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>hello {msg}!</h2>
+      {addResult(4, 3)}
+      <CountryList countries={list} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ tsc/src/CountryItem.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { CountryType } from "./App";
+
+type CountryItemPropsType = {
+    countryitem: CountryType;
+};
+
+const CountryItem  = (props:CountryItemPropsType) => {
+    let item = props.countryitem;
+
+    return (
+        <li
+            className={item.visited ? "list-item active" : "list-item"}
+        >
+            {item.country}
+        </li>
+    );
+};
+
+export default CountryItem;
