282b0424de619532cac245f519c0f41a90caf1a6
aran <aran@aranui-MacBookAir.local>
2023-02-22 11:10:53 +0900

    [todo-list]

---
+++ tsc/src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+
+const TodoList = ()  => {
+
+    return (
+        <ul className="todo-list">
+            <TodoListItem />
+        </ul>
+    )
+}
+
+export default TodoList;
---
+++ tsc/src/components/TodoListItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoListItem = ()  => {
+
+    return (
+        <li className="todo-item">
+            <p>aaa</p>
+        </li>
+    )
+}
+
+export default TodoListItem;
